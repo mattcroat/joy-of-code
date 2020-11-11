d82ca88f08fda5342079d2be06172fb726b7c3e4
mattcroat <mattcroat.dev@gmail.com>
2020-11-11 22:00:06 +0100

    Change return to inline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
 import { Flex, Heading } from '@chakra-ui/core'
 import Head from 'next/head'
 
-const Home = (): JSX.Element => {
-  return (
-    <>
-      <Head>
-        <title>Joy of Code</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Flex h="100vh" align="center" justify="center">
-        <Heading as="h1" size="2xl">
-          <span role="img" aria-label="artist palette">
-            🎨
-          </span>{' '}
-          Joy of Code
-        </Heading>
-      </Flex>
-    </>
-  )
-}
+const Home = (): JSX.Element => (
+  <>
+    <Head>
+      <title>Joy of Code</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Flex h="100vh" align="center" justify="center">
+      <Heading as="h1" size="2xl">
+        <span role="img" aria-label="artist palette">
+          🎨
+        </span>{' '}
+        Joy of Code
+      </Heading>
+    </Flex>
+  </>
+)
 
 export default Home
