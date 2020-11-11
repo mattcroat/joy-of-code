257f63f89d1d6940ce5c7c8f303fc06a8d33b43a
mattcroat <mattcroat.dev@gmail.com>
2020-11-11 22:01:45 +0100

    Add ColorModeProvider to enable dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
-import { ThemeProvider, CSSReset, Heading, Text, Code } from '@chakra-ui/core'
+import {
+  ThemeProvider,
+  ColorModeProvider,
+  CSSReset,
+  Heading,
+  Text,
+  Code,
+} from '@chakra-ui/core'
 import { MDXProvider } from '@mdx-js/react'
+
 import type { AppProps } from 'next/app'
 
 const components = {
@@ -9,15 +17,15 @@ const components = {
   code: Code,
 }
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return (
-    <MDXProvider components={components}>
-      <ThemeProvider>
+const App = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <ThemeProvider>
+    <ColorModeProvider value="dark">
+      <MDXProvider components={components}>
         <CSSReset />
         <Component {...pageProps} />
-      </ThemeProvider>
-    </MDXProvider>
-  )
-}
+      </MDXProvider>
+    </ColorModeProvider>
+  </ThemeProvider>
+)
 
 export default App
