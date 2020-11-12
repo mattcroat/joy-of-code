9bba587df2c3538a47cab48c053233c0dd879ed7
mattcroat <mattcroat.dev@gmail.com>
2020-11-12 23:57:59 +0100

    Remove placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading } from '@chakra-ui/core'
 import Head from 'next/head'
+
+import Navbar from '@/components/Navbar'
 
 const Home = (): JSX.Element => (
   <>
@@ -7,14 +8,7 @@ const Home = (): JSX.Element => (
       <title>Joy of Code</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Flex h="100vh" align="center" justify="center">
-      <Heading as="h1" size="2xl">
-        <span role="img" aria-label="artist palette">
-          🎨
-        </span>{' '}
-        Joy of Code
-      </Heading>
-    </Flex>
+    <Navbar />
   </>
 )
 
