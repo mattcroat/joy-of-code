642f04adc5e679e5ac0f88e19f172105fc7d344e
mattcroat <mattcroat.dev@gmail.com>
2020-11-15 13:52:09 +0100

    Add theme to provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { MDXProvider } from '@mdx-js/react'
 
 import type { AppProps } from 'next/app'
 
+import theme from '@/styles/theme'
+
 const components = {
   h1: Heading,
   h2: Heading,
@@ -11,7 +13,7 @@ const components = {
 }
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <MDXProvider components={components}>
       <Component {...pageProps} />
     </MDXProvider>
