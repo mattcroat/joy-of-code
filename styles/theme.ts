fc5916a05515caf59df875c57f1ff7f5ff9a279b
mattcroat <mattcroat.dev@gmail.com>
2020-11-16 15:35:42 +0100

    Change theme heading to Inter, and it's font weight to black

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,13 +3,13 @@ import { extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   fonts: {
     body: 'Inter, sans-serif',
-    heading: 'Libre Baskerville, serif',
+    heading: 'Inter, sans-serif',
     mono: 'Mononoki, monospace',
   },
   components: {
     Heading: {
       baseStyle: {
-        fontWeight: 'normal',
+        fontWeight: '900',
       },
     },
   },
