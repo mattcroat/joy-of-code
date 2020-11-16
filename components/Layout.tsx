af6a5cdffac099564686b0b75978d358acc01896
mattcroat <mattcroat.dev@gmail.com>
2020-11-16 13:40:41 +0100

    Add Layout.tsx container component

---
+++ components/Layout.tsx
@@ -0,0 +1,27 @@
+import { Box } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import Head from 'next/head'
+
+import Navigation from '@/components/Navigation'
+
+interface LayoutProps {
+  children?: ReactNode
+  title?: string
+}
+
+const Layout = ({
+  children,
+  title = 'Joy of Code',
+}: LayoutProps): JSX.Element => (
+  <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <Navigation />
+    <Box as="main" ml="80px" mt={8} px={8}>
+      {children}
+    </Box>
+  </>
+)
+
+export default Layout
