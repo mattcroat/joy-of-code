12a9a157e4a469aca6bcde78b716784de9aea432
mattcroat <mattcroat.dev@gmail.com>
2020-11-16 13:38:16 +0100

    Add meta tag, favicon, language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="/fonts/fonts.css" />
         </Head>
         <body>
