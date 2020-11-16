d0b4e78bee467a207969fe13140221706f2d0d5e
mattcroat <mattcroat.dev@gmail.com>
2020-11-16 15:46:30 +0100

    Move viewport meta tag to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,14 @@ import type { AppProps } from 'next/app'
 import MDXComponents from '@/components/MDXComponents'
 
 import theme from '@/styles/theme'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <ChakraProvider theme={theme}>
     <MDXProvider components={MDXComponents}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </MDXProvider>
   </ChakraProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="/fonts/fonts.css" />
         </Head>
