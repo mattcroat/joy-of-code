dca69f39846a94dac6acbf154a644178c29f829c
mattcroat <mattcroat.dev@gmail.com>
2020-11-17 22:40:46 +0100

    Clean up [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,37 +1,16 @@
 import fs from 'fs'
+import path from 'path'
+
 import matter from 'gray-matter'
 import hydrate from 'next-mdx-remote/hydrate'
 import renderToString from 'next-mdx-remote/render-to-string'
-import path from 'path'
 
 import { GetStaticPaths, GetStaticProps } from 'next'
 
 import Layout from '@/components/Layout'
+import MDXComponents from '@/components/MDXComponents'
 
 import { postsPath, postFilePaths } from '@/utils/posts'
-const PostPage = ({ source, frontMatter }): JSX.Element => {
-  const content = hydrate(source)
-
-  return <Layout title={frontMatter.title}>{content}</Layout>
-}
-
-export default PostPage
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postFilePath = path.join(postsPath, `${params?.slug}.mdx`)
-  const source = fs.readFileSync(postFilePath)
-
-  const { content, data } = matter(source)
-
-  const MDXSource = await renderToString(content)
-
-  return {
-    props: {
-      source: MDXSource,
-      frontMatter: data,
-    },
-  }
-}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = postFilePaths
@@ -43,3 +22,27 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: false,
   }
 }
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postFilePath = path.join(postsPath, `${params?.slug}.mdx`)
+  const source = fs.readFileSync(postFilePath)
+
+  const { content, data } = matter(source)
+
+  const MDXSource = await renderToString(content, { MDXComponents })
+
+  return {
+    props: {
+      MDXSource,
+      frontMatter: data,
+    },
+  }
+}
+
+const PostPage = ({ MDXSource, frontMatter }): JSX.Element => {
+  const content = hydrate(MDXSource, { MDXComponents })
+
+  return <Layout title={frontMatter.title}>{content}</Layout>
+}
+
+export default PostPage
