9a8d39e2ebcbc47bf92b4c1b38a887f1c0491acf
mattcroat <mattcroat.dev@gmail.com>
2020-11-17 22:41:56 +0100

    Add MDX components to work with next-mdx-remote

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,10 +1,46 @@
-import { Heading, Text, Code } from '@chakra-ui/react'
+import { Divider, Heading, Text } from '@chakra-ui/react'
+
+interface Children {
+  children: string
+}
+
+const MainHeading = (children: Children): JSX.Element => {
+  return (
+    <Heading
+      as="h1"
+      fontWeight="700"
+      color="gray.400"
+      letterSpacing="-1px"
+      {...children}
+    />
+  )
+}
+
+const SecondaryHeading = (children: Children): JSX.Element => {
+  return (
+    <Heading
+      as="h2"
+      fontWeight="700"
+      color="gray.400"
+      letterSpacing="-1px"
+      {...children}
+    />
+  )
+}
+
+const BodyText = (children: Children): JSX.Element => {
+  return <Text fontSize={20} lineHeight="1.6" mt={4} {...children} />
+}
+
+const Separator = (children: Children): JSX.Element => {
+  return <Divider h="2px" w="20px" bg="gray.400" mt={4} {...children} />
+}
 
 const MDXComponents = {
-  h1: Heading,
-  h2: Heading,
-  p: Text,
-  code: Code,
+  h1: MainHeading,
+  h2: SecondaryHeading,
+  p: BodyText,
+  hr: Separator,
 }
 
 export default MDXComponents
