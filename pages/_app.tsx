988db9605e411ae7bccea7bd0787d7965ab0fdd7
mattcroat <mattcroat.dev@gmail.com>
2020-11-19 23:32:43 +0100

    Add GlobalStyle component wrapper that injects styles based on the color mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,42 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, useColorMode } from '@chakra-ui/react'
+import { Global, css } from '@emotion/react'
 import { MDXProvider } from '@mdx-js/react'
 
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import MDXComponents from '@/components/MDXComponents'
 
+import { prismDarkTheme, prismLightTheme } from '@/styles/prism'
 import theme from '@/styles/theme'
-import Head from 'next/head'
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode()
+
+  return (
+    <>
+      <Global
+        styles={css`
+          ${colorMode === 'dark' ? prismDarkTheme : prismLightTheme};
+        `}
+      />
+      {children}
+    </>
+  )
+}
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <ChakraProvider theme={theme}>
     <MDXProvider components={MDXComponents}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
+      <GlobalStyle>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </GlobalStyle>
     </MDXProvider>
   </ChakraProvider>
 )
