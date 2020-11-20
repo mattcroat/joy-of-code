588133bb0a1b5c283c4d92f1ab30e8843615fedf
mattcroat <mattcroat.dev@gmail.com>
2020-11-20 21:31:43 +0100

    Clean up prism styles so they use theme defaults, remove unused styles

--- styles/prism.ts
+++ styles/prism.ts
@@ -52,11 +52,11 @@ const prismBaseTheme = css`
   .token.prolog,
   .token.doctype,
   .token.cdata {
-    color: hsl(210, 12.6%, 50.2%);
+    color: hsl(180, 8%, 54%);
   }
 
   .token.punctuation {
-    color: hsl(0, 0%, 60%);
+    color: ${theme.colors.blue[100]};
   }
 
   .token.namespace {
@@ -79,7 +79,7 @@ const prismBaseTheme = css`
   .token.char,
   .token.builtin,
   .token.inserted {
-    color: hsl(80, 100%, 30%);
+    color: ${theme.colors.red[200]};
   }
 
   .token.operator,
@@ -87,18 +87,18 @@ const prismBaseTheme = css`
   .token.url,
   .language-css .token.string,
   .style .token.string {
-    color: hsl(33, 45%, 42%);
+    color: ${theme.colors.red[200]};
   }
 
   .token.atrule,
   .token.attr-value,
   .token.keyword {
-    color: hsl(198, 100%, 33%);
+    color: ${theme.colors.orange[200]};
   }
 
   .token.function,
   .token.class-name {
-    color: hsl(348, 68%, 58%);
+    color: ${theme.colors.blue[200]};
   }
 
   .token.regex,
@@ -119,106 +119,10 @@ const prismBaseTheme = css`
   .token.entity {
     cursor: help;
   }
-
-  .mdx-marker {
-    display: block;
-    margin-left: -${theme.space[4]};
-    margin-right: -${theme.space[4]};
-    padding-left: ${theme.space[4]};
-    padding-right: ${theme.space[4]};
-    background-color: ${theme.colors.gray[200]};
-    box-shadow: inset 3px 0px 0 0px ${theme.colors.blue[600]};
-    min-width: fit-content;
-  }
-
-  .remark-code-title {
-    padding: ${theme.space[2]} ${theme.space[4]};
-    font-family: ${theme.fonts.mono};
-    background-color: ${theme.colors.gray[200]};
-    color: ${theme.colors.gray[800]};
-    border: 1px solid ${theme.colors.gray[200]};
-    border-top-left-radius: ${theme.radii.lg};
-    border-top-right-radius: ${theme.radii.lg};
-    font-size: 0.8rem;
-    font-weight: 600;
-    margin-bottom: 0;
-    width: 100%;
-
-    + pre {
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-      margin-top: 0;
-    }
-  }
 `
 
 export const prismDarkTheme = css`
   ${prismBaseTheme};
-
-  :not(pre) > code[class*='language-'] {
-    background-color: hsl(207, 95%, 8%);
-  }
-
-  .token.attr-name {
-    color: hsl(84, 62%, 63%);
-    font-style: italic;
-  }
-
-  .token.comment {
-    color: hsl(180, 8%, 54%);
-  }
-
-  .token.string,
-  .token.url {
-    color: hsl(84, 62%, 63%);
-  }
-
-  .token.variable {
-    color: hsl(217, 34%, 88%);
-  }
-
-  .token.number {
-    color: hsl(14, 90%, 70%);
-  }
-
-  .token.builtin,
-  .token.char,
-  .token.constant,
-  .token.function {
-    color: hsl(221, 100%, 75%);
-  }
-
-  .token.punctuation {
-    color: hsl(276, 68%, 75%);
-  }
-
-  .token.selector,
-  .token.doctype {
-    color: hsl(276, 68%, 75%);
-    font-style: 'italic';
-  }
-
-  .token.class-name {
-    color: hsl(33, 100%, 77%);
-  }
-
-  .token.tag,
-  .token.operator,
-  .token.keyword {
-    color: hsl(340, 100%, 83%);
-  }
-
-  .token.boolean {
-    color: hsl(340, 100%, 83%);
-  }
-
-  .token.property {
-    color: hsl(174, 42%, 65%);
-  }
-
-  .token.namespace {
-    color: hsl(197, 31%, 77%);
-  }
 
   code[class*='language-'],
   pre[class*='language-'] {
@@ -227,16 +131,10 @@ export const prismDarkTheme = css`
 
   :not(pre) > code[class*='language-'],
   pre[class*='language-'] {
-    background-color: ${theme.colors.gray[900]};
-  }
-
-  .mdx-marker {
-    background-color: ${theme.colors.gray[700]};
-  }
-
-  .remark-code-title {
-    background-color: ${theme.colors.gray[700]};
-    color: ${theme.colors.gray[100]};
+    background-color: ${theme.colors.gray[800]};
+    box-shadow: ${theme.shadows.lg};
+    border: 1px solid ${theme.colors.gray[900]};
+    border-radius: 4px;
   }
 `
 
@@ -251,9 +149,8 @@ export const prismLightTheme = css`
   :not(pre) > code[class*='language-'],
   pre[class*='language-'] {
     background-color: ${theme.colors.gray[50]};
-  }
-
-  .mdx-marker {
-    background-color: hsla(204, 45%, 96%, 1);
+    box-shadow: ${theme.shadows.lg};
+    border: 1px solid ${theme.colors.gray[100]};
+    border-radius: 4px;
   }
 `
