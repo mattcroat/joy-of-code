a9fa81e781d2567297960f935da4d527b0340a6c
mattcroat <mattcroat.dev@gmail.com>
2020-11-20 23:14:29 +0100

    Laying out groundwork for landing page cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
-import Head from 'next/head'
+import { Box, Divider, Heading, SimpleGrid } from '@chakra-ui/react'
 
-import Navigation from '@/components/Navigation'
+import Layout from '@/components/Layout'
 
-const Home = (): JSX.Element => (
-  <>
-    <Head>
-      <title>Joy of Code</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Navigation />
-  </>
+const IndexPage = (): JSX.Element => (
+  <Layout title="Joy of Code | Index">
+    <Box px={16}>
+      <Heading as="h1" color="gray.400" letterSpacing="-2px">
+        Posts
+      </Heading>
+
+      <Divider h="2px" w="20px" my={4} bg="gray.400" />
+
+      <SimpleGrid minChildWidth="480px" spacing={8}>
+        <Box
+          position="relative"
+          h="280px"
+          maxW="480px"
+          bg="linear-gradient(45deg, magenta, wheat)"
+          border="1px"
+          borderColor="gray.900"
+          borderRadius="4px"
+          boxShadow="lg"
+          overflow="hidden"
+        >
+          <Heading
+            as="h2"
+            position="absolute"
+            bottom={4}
+            left={4}
+            fontSize="4rem"
+            lineHeight="1"
+            color="white"
+            letterSpacing="-2px"
+            textShadow="4px 4px 0 rebeccapurple"
+          >
+            What are Closures in JavaScript?
+          </Heading>
+        </Box>
+      </SimpleGrid>
+    </Box>
+  </Layout>
 )
 
-export default Home
+export default IndexPage
