644fc5b544de9ecdfc7c00124d656b229eb1fa92
mattcroat <mattcroat.dev@gmail.com>
2020-11-20 21:29:02 +0100

    Add Main.tsx component

---
+++ components/Main.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+interface MainProps {
+  children: ReactNode
+}
+
+const Main = ({ children }: MainProps): JSX.Element => (
+  <Box as="main" ml="80px" mt={8}>
+    {children}
+  </Box>
+)
+
+export default Main
