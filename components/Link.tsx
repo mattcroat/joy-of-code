cbdd313c9acd19693fcf1cc5e8b510e969c469e4
mattcroat <mattcroat.dev@gmail.com>
2020-11-22 23:04:30 +0100

    Add Link.tsx component to wrap Link from Next.js to be able to pass Chakra UI props

---
+++ components/Link.tsx
@@ -0,0 +1,28 @@
+// ui components
+import { Link as ChakraLink } from '@chakra-ui/react'
+
+// next
+import NextLink from 'next/link'
+import { ReactNode } from 'react'
+
+// types
+interface LinkProps {
+  as: string
+  color: string
+  children: ReactNode
+  hover?: {
+    transition: string
+    color: string
+  }
+  href: string
+}
+
+const Link = ({ as, color, children, hover, href }: LinkProps): JSX.Element => (
+  <NextLink href={href} as={as} passHref>
+    <ChakraLink color={color} _hover={hover}>
+      {children}
+    </ChakraLink>
+  </NextLink>
+)
+
+export default Link
