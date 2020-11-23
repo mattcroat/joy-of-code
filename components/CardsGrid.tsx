71e0613d38fb14b93db08d9a993129d3f05dcdea
mattcroat <mattcroat.dev@gmail.com>
2020-11-23 20:50:01 +0100

    Change PostCard import to Card

--- components/CardsGrid.tsx
+++ components/CardsGrid.tsx
@@ -2,13 +2,13 @@
 import { Grid } from '@chakra-ui/react'
 
 // components
-import PostCard from '@/components/PostCard'
+import Card from '@/components/Card'
 
 const CardsGrid = (): JSX.Element => (
   <Grid gap={8} templateColumns="repeat(auto-fit, minmax(400px, 400px))">
-    <PostCard theme="js" title="What are Closures in JavaScript" />
-    <PostCard theme="react" title="React Hooks" />
-    <PostCard theme="web" title="Why Accessibility Matters" />
+    <Card theme="js" title="What are Closures in JavaScript" />
+    <Card theme="react" title="React Hooks" />
+    <Card theme="web" title="Why Accessibility Matters" />
   </Grid>
 )
 
