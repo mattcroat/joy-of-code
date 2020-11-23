ea33b7f88677972582d285b1c077da1a4c39c134
mattcroat <mattcroat.dev@gmail.com>
2020-11-23 15:48:23 +0100

    Rename MainHeadingProps to more generic HeadingProps, add SecondaryHeading, Divider

--- components/Headings.tsx
+++ components/Headings.tsx
@@ -1,16 +1,36 @@
 // ui components
-import { Divider, Heading } from '@chakra-ui/react'
+import { Divider as ChakraDivider, Heading } from '@chakra-ui/react'
 
 // types
-interface MainHeadingProps {
+interface HeadingProps {
   title: string
+  withDivider?: boolean
 }
 
-export const MainHeading = ({ title }: MainHeadingProps): JSX.Element => (
+const Divider = (): JSX.Element => (
+  <ChakraDivider h="4px" w="40px" mt={2} mb={4} bg="gray.600" />
+)
+
+export const MainHeading = ({
+  title,
+  withDivider,
+}: HeadingProps): JSX.Element => (
   <>
-    <Heading as="h1" color="gray.400" letterSpacing="-2px">
+    <Heading as="h1" fontSize="5xl" color="gray.400" letterSpacing="-2px">
       {title}
     </Heading>
-    <Divider h="2px" w="20px" my={4} bg="gray.400" />
+    {withDivider && <Divider />}
+  </>
+)
+
+export const SecondaryHeading = ({
+  title,
+  withDivider,
+}: HeadingProps): JSX.Element => (
+  <>
+    <Heading as="h2" color="gray.400" letterSpacing="-2px">
+      {title}
+    </Heading>
+    {withDivider && <Divider />}
   </>
 )
