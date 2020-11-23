c35167c47abe6f0fd01b628e5ac30e2cebb2cbb6
mattcroat <mattcroat.dev@gmail.com>
2020-11-23 16:37:46 +0100

    Add icon to card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,5 +1,12 @@
 // ui components
 import { Box, Heading } from '@chakra-ui/react'
+import {
+  faHtml5,
+  faJs,
+  faReact,
+  IconDefinition,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 // components
 import Link from '@/components/Link'
@@ -15,6 +22,7 @@ interface PostCardTheme {
   [key: string]: {
     bg: string
     color: string
+    icon: IconDefinition
   }
 }
 
@@ -23,14 +31,17 @@ const postCardTheme: PostCardTheme = {
   js: {
     bg: 'radial-gradient(circle, #FAF089, #ECC94B 100%)',
     color: 'black',
+    icon: faJs,
   },
   react: {
     bg: 'radial-gradient(circle, #4299e1, #2b6cb0 100%)',
     color: 'white',
+    icon: faReact,
   },
   web: {
     bg: 'radial-gradient(circle, #FBD38D, #ED8936 100%)',
     color: 'black',
+    icon: faHtml5,
   },
 }
 
@@ -46,6 +57,14 @@ const PostCard = ({ theme, title }: PostCardProps): JSX.Element => (
         boxShadow="lg"
         overflow="hidden"
       >
+        <Box
+          position="absolute"
+          top={4}
+          right={4}
+          color={postCardTheme[theme].color}
+        >
+          <FontAwesomeIcon icon={postCardTheme[theme].icon} size="2x" />
+        </Box>
         <Heading
           as="h2"
           maxW="380px"
