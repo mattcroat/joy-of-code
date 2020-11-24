9760f71320634a1dc07026ec63c34d5ea9b51c89
mattcroat <mattcroat.dev@gmail.com>
2020-11-24 16:32:21 +0100

    Add Image, Code components to MDXComponents

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,5 +1,7 @@
 import {
+  Code,
   Divider,
+  Image,
   Link,
   Text,
   MainHeading,
@@ -11,6 +13,8 @@ const MDXComponents = {
   h1: MainHeading,
   h2: SecondaryHeading,
   hr: Divider,
+  img: Image,
+  inlineCode: Code,
   p: Text,
 }
 
