6fd36a70bc49849788b450ab261fbeea1e5fe220
mattcroat <mattcroat.dev@gmail.com>
2020-11-25 14:27:50 +0100

    Refactor SlideFade to more generic Slide component that accepts variants, add slideRightVariants

--- components/Transitions.tsx
+++ components/Transitions.tsx
@@ -4,12 +4,13 @@ import { motion } from 'framer-motion'
 // types
 import { ReactNode } from 'react'
 
-interface SlideFadeProps {
+interface SlideProps {
   children: ReactNode
+  right?: boolean
 }
 
 // variants
-const opacityVariants = {
+const slideVariants = {
   hidden: { opacity: 0, y: -100 },
   visible: {
     opacity: 1,
@@ -21,8 +22,24 @@ const opacityVariants = {
   },
 }
 
-export const SlideFade = ({ children }: SlideFadeProps): JSX.Element => (
-  <motion.div initial="hidden" animate="visible" variants={opacityVariants}>
+const slideRightVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.3,
+      ease: 'easeOut',
+    },
+  },
+}
+
+export const Slide = ({ children, right }: SlideProps): JSX.Element => (
+  <motion.div
+    initial="hidden"
+    animate="visible"
+    variants={!right ? slideVariants : slideRightVariants}
+  >
     {children}
   </motion.div>
 )
