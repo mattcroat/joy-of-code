68d2b18265c627b1c520b44aba669e20fc05f3ac
mattcroat <mattcroat.dev@gmail.com>
2020-11-25 19:26:16 +0100

    Replace Grid with MotionGrid component, change responsive styles

--- components/CardsGrid.tsx
+++ components/CardsGrid.tsx
@@ -1,18 +1,36 @@
 // ui components
 import { Grid } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 
 // components
 import Card from '@/components/Card'
 
+// motion components
+const MotionGrid = motion.custom(Grid)
+
+// variants
+const cardsGridVariant = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+}
+
 const CardsGrid = (): JSX.Element => (
-  <Grid
+  <MotionGrid
     gap={8}
-    templateColumns={[null, 'repeat(auto-fit, minmax(400px, 400px))']}
+    templateColumns={{ md: 'repeat(auto-fit, minmax(400px, 400px))' }}
+    animate="show"
+    initial="hidden"
+    variants={cardsGridVariant}
   >
-    <Card theme="js" title="What are Closures in JavaScript" />
-    <Card theme="react" title="React Hooks" />
-    <Card theme="web" title="Why Accessibility Matters" />
-  </Grid>
+    <Card theme="js" title="Super Long JavaScript Card Title" />
+    <Card theme="react" title="React Card Title" />
+    <Card theme="web" title="Web Card Title" />
+  </MotionGrid>
 )
 
 export default CardsGrid
