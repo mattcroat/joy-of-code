e741b380b71121485cb743259cba5bb3c786dbee
mattcroat <mattcroat.dev@gmail.com>
2020-11-26 20:21:11 +0100

    Add getStaticProps to pass posts to IndexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,31 @@ import CardsGrid from '@/components/CardsGrid'
 import Layout from '@/components/Layout'
 import { MainHeading, SecondaryHeading } from '@/components/UIComponents'
 
-const IndexPage = (): JSX.Element => (
+// utils
+import { getPosts } from '@/utils/posts'
+
+// types
+import { GetStaticProps } from 'next'
+
+interface IndexPageProps {
+  posts: {
+    title: string
+    description: string
+    published: string
+    category: string[]
+  }
+}
+
+// get posts metadata
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      posts: getPosts,
+    },
+  }
+}
+
+const IndexPage = ({ posts }: IndexPageProps): JSX.Element => (
   <Layout title="Joy of Code | ☕ Freshly Brewed Web Development Content">
     <Box px={[8, 16]}>
       <Box>
