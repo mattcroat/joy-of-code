d1617411e60cc4840eae77806a8651047d8cd2d3
mattcroat <mattcroat.dev@gmail.com>
2020-11-26 20:18:26 +0100

    Add getPosts to be a single source of truth for every post

--- utils/posts.ts
+++ utils/posts.ts
@@ -1,8 +1,21 @@
+// node
 import fs from 'fs'
 import path from 'path'
+
+// mdx
+import matter from 'gray-matter'
 
 export const postsPath = path.join(process.cwd(), 'posts')
 
 export const postFilePaths = fs
   .readdirSync(postsPath)
   .filter((path) => /\.mdx?$/.test(path))
+
+export const getPosts = postFilePaths.map((slug) => {
+  const postPath = `${postsPath}/${slug}`
+
+  const fileContents = fs.readFileSync(postPath, 'utf-8')
+  const { data: frontMatter } = matter(fileContents)
+
+  return frontMatter
+})
