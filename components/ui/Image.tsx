c6bbe59c9889241ac26b0ac42d55124640606802
mattcroat <mattcroat.dev@gmail.com>
2020-12-03 19:46:43 +0100

    Add fade in animation to images

--- components/ui/Image.tsx
+++ components/ui/Image.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { Box } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 import NextImage from 'next/image'
 
 interface Props {
@@ -7,16 +8,31 @@ interface Props {
   src: string
 }
 
+const MotionBox = motion.custom(Box)
+
+const ImageVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+    },
+  },
+}
+
 const Image: FC<Props> = ({ alt, src }) => (
-  <Box
+  <MotionBox
     position="relative"
     h={{ base: '400px', lg: '600px' }}
     w={{ xl: '140%' }}
     mx={{ xl: '-20%' }}
     my={{ sm: 8 }}
+    initial="hidden"
+    animate="show"
+    variants={ImageVariants}
   >
     <NextImage alt={alt} src={src} layout="fill" objectFit="contain" />
-  </Box>
+  </MotionBox>
 )
 
 export default Image
