5885800cb62ad625e1ee8733bac9e2ab0bfe0ceb
mattcroat <mattcroat.dev@gmail.com>
2020-12-17 22:22:59 +0100

    feat: Add Cloudinary as image source

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 })
