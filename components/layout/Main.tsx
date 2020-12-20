80944242890e32c6fbca43b82abc6232e63d40e0
mattcroat <mattcroat.dev@gmail.com>
2020-12-20 19:07:34 +0100

    refactor: Use named function

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { Box } from '@chakra-ui/react'
 
 import { Slide } from '@/components/motion'
@@ -7,12 +7,12 @@ interface Props {
   children: ReactNode
 }
 
-const Main: FC<Props> = ({ children }) => (
-  <Slide>
-    <Box as="main" ml={{ sm: '80px' }} pt={6} transition="color 0.2s">
-      {children}
-    </Box>
-  </Slide>
-)
-
-export default Main
+export function Main({ children }: Props) {
+  return (
+    <Slide>
+      <Box as="main" ml={{ sm: '80px' }} pt={6} transition="color 0.2s">
+        {children}
+      </Box>
+    </Slide>
+  )
+}
