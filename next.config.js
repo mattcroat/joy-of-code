7f4e4ba7e0d6a05f7943dfe824f66ea1e1133380
mattcroat <mattcroat.dev@gmail.com>
2021-02-09 21:42:49 +0100

    feat: Replace React with Preact in production

--- next.config.js
+++ next.config.js
@@ -4,4 +4,17 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  webpack: (config, { dev, isServer }) => {
+    // replace React with Preact in production build
+    // credit goes to https://leerob.io/blog/tailwind
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 })
