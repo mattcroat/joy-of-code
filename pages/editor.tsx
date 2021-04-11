1161bd75e69a5fe60a770032127c744d5eab9b2b
mattcroat <mattcroat.dev@gmail.com>
2021-04-11 18:34:17 +0200

    feat: Hide editor :detective:

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -3,3 +3,21 @@ import { Editor } from '@/root/components/screens/Editor'
 export default function EditorPage() {
   return <Editor />
 }
+
+export async function getServerSideProps() {
+  const production = process.env.NODE_ENV === 'production'
+
+  // secret hidden route 🤫
+  if (production) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
