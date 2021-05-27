75fa49d1af77c8bcb559a291d62a19d5e60c29a8
mattcroat <mattcroat.dev@gmail.com>
2021-05-27 21:37:12 +0200

    chore: Rename page

---
+++ pages/next.tsx
@@ -0,0 +1,28 @@
+import { getPostsByCategory, getSortedPosts } from '@/root/utils/helpers/posts'
+import { Category } from '@/root/components/screens/Category'
+
+interface Props {
+  category: string
+  posts: {
+    title: string
+    description: string
+    published: string
+    category: string
+    slug: string
+  }[]
+}
+
+export default function NextPage({ category, posts }: Props) {
+  return <Category category={category} posts={posts} title={category} />
+}
+
+export async function getStaticProps() {
+  const filteredPosts = getPostsByCategory('Next')
+
+  return {
+    props: {
+      category: 'Next',
+      posts: getSortedPosts(filteredPosts),
+    },
+  }
+}
