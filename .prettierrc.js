383e9c510fb1cba382e4b735079e9997bf1f77b3
mattcroat <mattcroat.dev@gmail.com>
2021-05-29 20:27:20 +0200

    chore: Format Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    printWidth: 80,
-    semi: false,
-    singleQuote: true,
-    trailingComma: 'es5',
-    tabWidth: 2,
-    useTabs: false,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
 }
