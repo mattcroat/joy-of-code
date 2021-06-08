abc4bd4d11bfef3f0b68f82023c2f595f6547043
mattcroat <mattcroat.dev@gmail.com>
2021-06-08 23:00:11 +0200

    refactor: Remove getServerSideProps from Editor

--- components/screens/Editor/Editor.tsx
+++ components/screens/Editor/Editor.tsx
@@ -5,21 +5,3 @@ import { PostImage } from '@/root/components/screens/Editor/PostImage'
 export function Editor() {
   return <PostImage />
 }
-
-export async function getServerSideProps() {
-  const production = process.env.NODE_ENV === 'production'
-
-  // secret hidden route 🤫
-  if (production) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: {},
-  }
-}
