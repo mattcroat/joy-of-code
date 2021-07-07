c233fdef3e8fae9d6c641a4f43a238f76ea9f5c5
mattcroat <mattcroat.dev@gmail.com>
2021-07-07 21:26:49 +0200

    feat: Lazy load iframe

--- components/mdx/Iframe.tsx
+++ components/mdx/Iframe.tsx
@@ -1,4 +1,7 @@
 import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+import { useIntersectionObserver } from '@/root/hooks/useIntersectionObserver'
 
 interface IframeProps {
   height: string
@@ -7,16 +10,29 @@ interface IframeProps {
 }
 
 export function Iframe({ height = '600px', src, title }: IframeProps) {
+  const [isVisible, setIsVisible] = React.useState<boolean>(false)
+  const iframeRef = React.useRef<HTMLDivElement>(null)
+
+  useIntersectionObserver({
+    target: iframeRef,
+    onIntersect: () => setIsVisible(true),
+    enabled: true,
+  })
+
   return (
-    <Box
-      as="iframe"
-      bg="white"
-      h={height}
-      mx={{ xl: '-10%' }}
-      my={{ sm: 8 }}
-      src={src}
-      title={title}
-      w={{ base: '100%', xl: '120%' }}
-    />
+    <Box ref={iframeRef}>
+      {isVisible && (
+        <Box
+          as="iframe"
+          bg="white"
+          h={height}
+          mx={{ xl: '-10%' }}
+          my={{ sm: 8 }}
+          src={src}
+          title={title}
+          w={{ base: '100%', xl: '120%' }}
+        />
+      )}
+    </Box>
   )
 }
