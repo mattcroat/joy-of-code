5975d7f8e7d43a396a19f3335c37fb5975764c44
mattcroat <mattcroat.dev@gmail.com>
2021-07-18 23:04:51 +0200

    feat: Add PreferencesProvider

---
+++ context/PreferencesProvider.tsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+import type { Dispatch, SetStateAction } from 'react'
+
+type PreferencesContext = {
+  accessibleFont: boolean
+  setAccessibleFont: Dispatch<SetStateAction<boolean>>
+}
+
+const PreferencesContext = createContext<PreferencesContext | undefined>(
+  undefined
+)
+
+export function PreferencesProvider({ children }: any) {
+  const [accessibleFont, setAccessibleFont] = useState<boolean>(() => {
+    if (typeof window !== 'undefined') {
+      if (window.localStorage.getItem('accessibleFont')) {
+        return true
+      }
+    }
+
+    return false
+  })
+
+  useEffect(() => {
+    const storage = window.localStorage
+    const serialize = JSON.stringify
+
+    if (accessibleFont) {
+      storage.setItem('accessibleFont', serialize(true))
+    } else {
+      storage.removeItem('accessibleFont')
+    }
+  }, [accessibleFont])
+
+  return (
+    <PreferencesContext.Provider value={{ accessibleFont, setAccessibleFont }}>
+      {children}
+    </PreferencesContext.Provider>
+  )
+}
+
+export function usePreferences() {
+  const context = useContext(PreferencesContext)
+
+  if (context === undefined) {
+    throw new Error(
+      '💩 usePreferences must be used within a PreferencesProvider.'
+    )
+  }
+
+  return context
+}
