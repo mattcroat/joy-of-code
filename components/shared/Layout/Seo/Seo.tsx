f66f9aea9e807b48c83d4e6019d588e1310ff72f
mattcroat <mattcroat.dev@gmail.com>
2021-08-22 17:18:25 +0200

    chore: Update site description

--- components/shared/Layout/Seo/Seo.tsx
+++ components/shared/Layout/Seo/Seo.tsx
@@ -10,7 +10,7 @@ export function Seo({ ...metadata }: SeoProps) {
 
   const meta = {
     title: 'Joy of Code',
-    description: `Web development tutorials, and articles`,
+    description: `Joy of Code is a digital garden about web development tutorials, and articles such as HTML, CSS, JavaScript, React and TypeScript.`,
     image: 'https://joyofcode.xyz/images/og-image.webp',
     type: 'website',
     ...metadata,
