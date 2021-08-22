57a81348da2465f06b5aa97fe56cd249d3d3f49b
mattcroat <mattcroat.dev@gmail.com>
2021-08-22 22:30:03 +0200

    build: Update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   webpack: (config, { dev, isServer }) => {
-    if (isServer) {
-      require('./utils/scripts/generate-rss')
-    }
-
     // replace React with Preact in production build
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
