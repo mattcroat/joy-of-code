9f4e5bf381b1749d8a7fd70ec87ca6a289c78a6e
mattcroat <mattcroat.dev@gmail.com>
2021-08-26 22:02:31 +0200

    fix: Typo

--- components/screens/Newsletter/Newsletter.tsx
+++ components/screens/Newsletter/Newsletter.tsx
@@ -9,10 +9,7 @@ export function Newsletter() {
       <hr className="w-10 h-1 my-2 bg-gray-600 border-0"></hr>
 
       <div className="max-w-[90ch] mt-8 space-y-8">
-        <p>
-          Rreceive occasional updates to get notified when I release a new post,
-          or what I'm up to.
-        </p>
+        <p>Receive updates to get notified when I release a new post.</p>
         <MailingList />
       </div>
     </Layout>
