eb8d5d14558ff49697267d0d290a5aea953b0f92
mattcroat <mattcroat.dev@gmail.com>
2021-09-01 15:54:05 +0200

    refactor: Posts utils

--- utils/posts.ts
+++ utils/posts.ts
@@ -1,43 +1,36 @@
-import fs from 'fs'
+import { readdirSync, readFileSync } from 'fs'
 import matter from 'gray-matter'
 import path from 'path'
 
-interface Post {
-  category: string
-  description: string
-  published: number
-  slug: string
-  title: string
-}
+import type { PostType } from '@/root/types/post'
 
 export const postsPath = path.join(process.cwd(), 'posts')
 
-export const postFilePaths = fs
-  .readdirSync(postsPath)
-  .filter((path) => /\.mdx?$/.test(path))
+export const postFilePaths = readdirSync(postsPath).filter((path) =>
+  /\.mdx?$/.test(path)
+)
 
 export const getPosts = postFilePaths.map((slug) => {
   const postPath = `${postsPath}/${slug}`
 
-  const fileContents = fs.readFileSync(postPath, 'utf-8')
+  const fileContents = readFileSync(postPath, 'utf-8')
   const { data: frontMatter } = matter(fileContents)
 
   frontMatter.published = new Date(frontMatter.published).valueOf()
   frontMatter.slug = slug.replace(/\.mdx?$/, '')
 
   return frontMatter
-}) as Array<Post>
+}) as Array<PostType>
 
 export function getPostsByCategory(category: string) {
   const filteredPosts = getPosts.filter(
     (post) => post.category.toLowerCase() === category.toLowerCase()
   )
-
   return filteredPosts
 }
 
-export function getSortedPosts(posts: Array<Post>) {
+export function getSortedPosts(posts: Array<PostType>) {
   if (!posts) return
-  const sortedPosts = posts.sort((a, b) => b.published - a.published)
+  const sortedPosts = posts.sort((a, b) => +b.published - +a.published)
   return sortedPosts
 }
