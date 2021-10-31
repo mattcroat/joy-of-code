d8a3ce325b920ba0f5d700c87512ff5ff1db6f2b
mattcroat <mattcroat.dev@gmail.com>
2021-10-31 19:27:12 +0100

    style: Theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // card
+        'card-title': 'var(--cardTitle)',
         // site
         highlight: 'var(--highlight)',
         muted: 'var(--muted)',
         primary: 'var(--primary)',
         secondary: 'var(--secondary)',
         body: 'var(--body)',
-        highlightText: 'var(--highlightText)',
-        selection: 'var(--selection)',
-        selectionText: 'var(--selectionText)',
         separator: 'var(--separator)',
-        // card
-        cardTitle: 'var(--cardTitle)',
         // code
-        codeTitle: 'var(--code-title)',
-        codeBorder: 'var(--code-border)',
         code: 'var(--code)',
         pre: 'var(--pre)',
-        tokenFunction: 'var(--tokenFunction)',
-        tokenKeyword: 'var(--tokenKeyword)',
-        tokenOperator: 'var(--tokenOperator)',
-        tokenPunctuation: 'var(--tokenPunctuation)',
-        tokenString: 'var(--tokenString)',
-        comment: 'var(--comment)',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
