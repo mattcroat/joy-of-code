ca26428b7b0f51b1c42bdce1f8e0ebcaf9be1d0e
mattcroat <mattcroat.dev@gmail.com>
2021-12-09 21:39:51 +0100

    build: Update Next.js config to not include Preact

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   experimental: { esmExternals: true },
-  webpack: (config, { dev, isServer }) => {
-    // replace React with Preact in production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
 }
