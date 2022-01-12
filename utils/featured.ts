645093838747f856bada77ff9d422ce2f7d3cab4
mattcroat <mattcroat.dev@gmail.com>
2022-01-12 20:51:56 +0100

    chore: Update featured

--- utils/featured.ts
+++ utils/featured.ts
@@ -1,7 +1,6 @@
 export const featuredPosts = [
   'TypeScript Fundamentals',
   'Create a Code Sandbox',
-  'Create a Markdown Blog Using Next.js',
   'Design For Developers',
   'Svelte For Beginners',
 ]
