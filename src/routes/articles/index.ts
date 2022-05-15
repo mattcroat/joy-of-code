393c3b1891db16f565d38e256e405d928d39b08a
mattcroat <mattcroat.dev@gmail.com>
2022-05-15 14:48:30 +0200

    fix: Sort articles

--- src/routes/articles/index.ts
+++ src/routes/articles/index.ts
@@ -5,9 +5,16 @@ import { getPosts } from '$root/lib/posts'
 export const get: RequestHandler = async () => {
   const { posts } = await getPosts()
 
+  const sortedPosts = posts.sort((firstElement, secondElement) => {
+    return (
+      new Date(secondElement.published).getTime() -
+      new Date(firstElement.published).getTime()
+    )
+  })
+
   return {
     status: 200,
-    body: { posts },
+    body: { posts: sortedPosts },
     headers: {
       'Cache-Control': `public, max-age=${60 * 60}, s-maxage=${60 * 60}`,
     },
