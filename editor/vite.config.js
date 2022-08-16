3603ebda054da61455744cc6ec84ef56113fd7d0
Matia <38083522+mattcroat@users.noreply.github.com>
2022-08-16 12:32:23 +0200

    feat: Decouple editor (#73)

--- editor/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { webSocketServer } from './socket/plugin.js'
-
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [sveltekit(), webSocketServer],
-}
-
-export default config
