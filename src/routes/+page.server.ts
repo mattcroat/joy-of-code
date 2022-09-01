650c4e1e6a4a83dec4324ea1cb764e639fc2599e
mattcroat <mattcroat.dev@gmail.com>
2022-09-01 14:51:11 +0200

    chore: Remove cache-control comment

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,6 @@ import { getPosts } from '$root/lib/posts'
 export const load: PageServerLoad = async ({ setHeaders }) => {
 	const posts = await getPosts()
 
-	// todo: cache control headers
 	setHeaders({
 		'Cache-Control': `public, max-age=${60 * 60}, s-maxage=${60 * 60}`,
 	})
