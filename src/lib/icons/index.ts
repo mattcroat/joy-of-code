7c3a073d0928bf0ec3f6f42b0abce095c672a572
mattcroat <mattcroat.dev@gmail.com>
2023-09-16 21:58:03 +0200

    refactor: Use Melt UI

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,21 +1,21 @@
-import Envelope from './envelope.svelte'
-import Eye from './eye.svelte'
-import Menu from './menu.svelte'
-import Cog from './cog.svelte'
 import ArrowRight from './arrow-right.svelte'
-import PencilSquare from './pencil-square.svelte'
-import Heart from './heart.svelte'
 import ChevronDoubleLeft from './chevron-double-left.svelte'
 import ChevronDoubleRight from './chevron-double-right.svelte'
+import Cog from './cog.svelte'
+import Envelope from './envelope.svelte'
+import Eye from './eye.svelte'
+import Heart from './heart.svelte'
+import Menu from './menu.svelte'
+import PencilSquare from './pencil-square.svelte'
 
 export {
-	Envelope,
-	Eye,
-	Menu,
-	Cog,
 	ArrowRight,
-	PencilSquare,
-	Heart,
 	ChevronDoubleLeft,
 	ChevronDoubleRight,
+	Cog,
+	Envelope,
+	Eye,
+	Heart,
+	Menu,
+	PencilSquare,
 }
--- src/lib/ui/header/preferences/index.svelte
+++ src/lib/ui/header/preferences/index.svelte
@@ -1,105 +1,79 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition'
-	import {
-		Popover,
-		PopoverButton,
-		PopoverPanel,
-	} from '@rgossiaux/svelte-headlessui'
+	import { createPopover } from '@melt-ui/svelte'
 	import { Cog } from '$lib/icons'
+	import { sounds } from '$lib/stores/sfx'
 
 	import Themes from './themes.svelte'
 	import Reading from './reading.svelte'
 	import Dyslexic from './dyslexic.svelte'
 	import Reset from './reset.svelte'
 
-	import { sounds } from '$lib/stores/sfx'
+	const {
+		elements: { trigger, content },
+		states: { open },
+	} = createPopover({
+		arrowSize: 16,
+		positioning: {
+			gutter: 20,
+		},
+	})
 </script>
 
-<div class="container">
-	<Popover let:open class="popover">
-		<PopoverButton aria-label="Preferences" on:click={() => $sounds.click()}>
-			<Cog width={24} height={24} aria-hidden={true} />
-		</PopoverButton>
+<button
+	{...$trigger}
+	use:trigger
+	on:click={() => $sounds.click()}
+	aria-label="Categories"
+>
+	<Cog width={24} height={24} aria-hidden={true} />
+</button>
 
-		{#if open}
-			<div transition:fade={{ duration: 100 }}>
-				<PopoverPanel class="popover-panel" static>
-					<div class="preferences">
-						<svg
-							width="24"
-							height="24"
-							class="arrow"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								id="inside"
-								d="M23 24H1L11.0909 1.98341C11.4474 1.20562 12.5526 1.20562 12.9091 1.98341L23 24Z"
-								fill="none"
-							/>
-							<path
-								id="outside"
-								d="M12.8944 1.78885L24 24H23L12.9021 2.88628C12.5396 2.12822 11.4604 2.12822 11.0979 2.88628L1 24H0L11.1056 1.78885C11.4741 1.05181 12.5259 1.0518 12.8944 1.78885Z"
-								fill="none"
-							/>
-						</svg>
+{#if open}
+	<div {...$content} use:content transition:fade={{ duration: 100 }}>
+		<div class="preferences">
+			<svg
+				width="24"
+				height="24"
+				class="arrow"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					id="inside"
+					d="M23 24H1L11.0909 1.98341C11.4474 1.20562 12.5526 1.20562 12.9091 1.98341L23 24Z"
+					fill="none"
+				/>
+				<path
+					id="outside"
+					d="M12.8944 1.78885L24 24H23L12.9021 2.88628C12.5396 2.12822 11.4604 2.12822 11.0979 2.88628L1 24H0L11.1056 1.78885C11.4741 1.05181 12.5259 1.0518 12.8944 1.78885Z"
+					fill="none"
+				/>
+			</svg>
 
-						<span class="title">Preferences</span>
+			<span class="title">Preferences</span>
 
-						<div class="options">
-							<Themes />
-							<Reading />
-							<Dyslexic />
-							<Reset />
-						</div>
-					</div>
-				</PopoverPanel>
+			<div class="options">
+				<Themes />
+				<Reading />
+				<Dyslexic />
+				<Reset />
 			</div>
-		{/if}
-	</Popover>
-</div>
+		</div>
+	</div>
+{/if}
 
 <style>
-	.container {
-		width: 24px;
-		height: 24px;
-		z-index: 10;
-	}
-
-	.container :global(.popover) {
-		height: 100%;
-		position: relative;
-	}
-
-	.container :global(.popover-panel) {
-		position: absolute;
-		top: 48px;
-		right: -22px;
-		z-index: 10;
-	}
-
 	.preferences {
+		position: relative;
 		background-image: var(--clr-menu-bg);
 		padding: var(--spacing-24);
 		border-top: 1px solid var(--clr-menu-border);
 		border-left: 1px solid var(--clr-menu-border);
 		border-radius: var(--rounded-20);
 		box-shadow: var(--shadow-lg);
-	}
-
-	.preferences .arrow {
-		position: absolute;
-		top: -23px;
-		right: 22px;
-	}
-
-	.preferences .arrow #inside {
-		fill: var(--clr-menu-arrow-bg);
-	}
-
-	.preferences .arrow #outside {
-		fill: var(--clr-menu-border);
+		z-index: 20;
 	}
 
 	.preferences .title {
