63d1b7f0cfbe3f816062e5afae8886b4f38cde8e
Matia <38083522+mattcroat@users.noreply.github.com>
2024-02-10 15:54:03 +0100

    build: Update to SvelteKit 2.0 (#160)
    * build: Update dependencies
    
    * chore: Update SvelteKit, TypeScript, and Vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel'
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
-	},
+		adapter: adapter()
+	}
 }
 
 export default config
