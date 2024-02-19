7cbf0492a6e3e0963a2e94dfb646ac23a5c5ad52
mattcroat <mattcroat.dev@gmail.com>
2024-02-19 21:16:29 +0100

    fix: Search position

--- src/lib/ui/header/search/search.svelte
+++ src/lib/ui/header/search/search.svelte
@@ -109,11 +109,12 @@
 		max-width: 600px;
 		position: fixed;
 		left: 50%;
-		top: 50%;
-		translate: -50% -50%;
+		top: 20%;
+		translate: -50% -0%;
 		border-radius: var(--rounded-4);
 		color: var(--clr-menu-text);
 		background-image: var(--clr-menu-bg);
+		box-shadow: 0px 0px 20px hsl(0 0% 0% / 40%);
 		overflow: hidden;
 		z-index: 40;
 
@@ -131,7 +132,7 @@
 	}
 
 	.results {
-		max-height: 80vh;
+		max-height: 60vh;
 		padding: var(--spacing-16);
 		overflow-y: auto;
 		scrollbar-width: thin;
@@ -164,6 +165,13 @@
 		border-top: 1px solid var(--clr-menu-border);
 		border-left: 1px solid var(--clr-menu-border);
 		transition: color 0.3s ease;
+
+		& kbd {
+			padding: 4px 8px;
+			background-color: gray;
+			border-radius: var(--rounded-4);
+			background-color: hsl(216 7% 20%);
+		}
 
 		&:hover {
 			color: var(--clr-primary);
