e1394fca2f94685e41ce2a55fc3a94004895cfc3
pvbhanuteja <pvbhanuteja@gmail.com>
2024-02-21 01:50:43 -0800

    fix rss double slash

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -24,8 +24,8 @@ export async function GET() {
 						<item>
 							<title>${post.title}</title>
 							<description>${post.description}</description>
-							<link>${config.siteUrl}/${post.slug}</link>
-							<guid isPermaLink="true">${config.siteUrl}/${post.slug}</guid>
+							<link>${config.siteUrl}${post.slug}</link>
+							<guid isPermaLink="true">${config.siteUrl}${post.slug}</guid>
 							<pubDate>${new Date(post.published).toUTCString()}</pubDate>
 						</item>
 					`
