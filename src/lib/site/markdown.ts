8fa1aaf83163afa22b14ae6ea9bdcdabe5b8f8f1
mattcroat <mattcroat.dev@gmail.com>
2024-04-24 11:06:52 +0200

    refactor: Markdown processor

--- src/lib/site/markdown.ts
+++ src/lib/site/markdown.ts
@@ -1,22 +1,17 @@
-// @ts-nocheck
-
-import { unified } from 'unified'
-import fromMarkdown from 'remark-parse'
-import fromMarkdownToHtml from 'remark-rehype'
-import parseHtmlAndMarkdown from 'rehype-raw'
-import toHtml from 'rehype-stringify'
 import matter from 'gray-matter'
-
-// plugins
+import { unified } from 'unified'
+import toMarkdownAST from 'remark-parse'
+import toHtmlAST from 'remark-rehype'
+import parseHtmlAndMarkdown from 'rehype-raw'
+import toHtmlString from 'rehype-stringify'
 import remarkGfm from 'remark-gfm'
-import remarkHeadings from 'remark-autolink-headings'
-import remarkSlug from 'remark-slug'
 import remarkSmartypants from 'remark-smartypants'
 import remarkTableofContents from 'remark-toc'
-import rehypePrism from 'rehype-prism-plus'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeCodeTitles from 'rehype-code-titles'
+import rehypePrism from 'rehype-prism-plus'
 import { rehypeCopyCode, rehypeUnwrapImages } from './plugins'
-
 import * as config from './config'
 import type { Frontmatter } from '$lib/types'
 
@@ -25,7 +20,7 @@ type ContentType = {
 	frontmatter: Frontmatter
 }
 
-function searchAndReplace(content: string, slug: string): string {
+function searchAndReplace(content: string, slug: string) {
 	const embed = /{% embed src="(.*?)" title="(.*?)" %}/g
 	const video = /{% video src="(.*?)" %}/g
 	const image = /{% img src="(.*?)" alt="(.*?)" %}/g
@@ -67,56 +62,31 @@ function searchAndReplace(content: string, slug: string): string {
 		})
 }
 
-export async function markdownToHTML(markdown: string): Promise<ContentType> {
-	const { content, data } = matter(markdown)
-
-	// I could use `compile` from mdsvex to get
-	// Svelte components working inside Markdown
-	const result = await unified()
-		.use(fromMarkdown)
+async function parseMarkdown(text: string, slug: string) {
+	const processor = await unified()
+		.use(toMarkdownAST)
 		.use([
-			// GitHub flavored Markdown
 			remarkGfm,
-
-			// Unique identifier for headings
-			remarkHeadings,
-
-			// Links for headings
-			remarkSlug,
-
-			// Typographic punctuation like real quotes
 			remarkSmartypants,
-
-			// Generates table of contents from headings
-			// `tight` removes <p> from <li> when nested
 			[remarkTableofContents, { tight: true }],
 		])
-		// To be able to parse a mix of Markdown and HTML
-		// `remark-rehype` is required with `rehype-raw`
-		// https://github.com/rehypejs/rehype-raw
-		.use(fromMarkdownToHtml, { allowDangerousHtml: true })
-
-		// Adds code titles above code blocks
+		.use(toHtmlAST, { allowDangerousHtml: true })
+		.use([rehypeSlug, rehypeAutolinkHeadings])
 		.use(rehypeCodeTitles)
-
-		// Adds syntax highlight, line numbers and higlight
 		.use(rehypePrism)
-
-		// For further processing turn content into a regular syntax tree
 		.use(parseHtmlAndMarkdown)
-
-		// Remove paragraph around images
 		.use(rehypeUnwrapImages)
-
-		// Copy code to clipboard
 		.use(rehypeCopyCode)
+		.use(toHtmlString, { allowDangerousHtml: true })
+		.process(searchAndReplace(text, slug))
+	return processor.value
+}
 
-		.use(toHtml)
-		.process(searchAndReplace(content, data.slug))
-	const processedMarkdown = result.value
+export async function markdownToHTML(text: string) {
+	const { content, data } = matter(text)
 
 	return {
-		content: processedMarkdown as string,
+		content: await parseMarkdown(content, data.slug),
 		frontmatter: data as Frontmatter,
 	}
 }
