5397672b7e2c22f85e77697e3d0be67c1d14b7ac
mattcroat <mattcroat.dev@gmail.com>
2024-06-02 18:38:09 +0200

    fix: drafts

--- src/routes/drafts/[slug]/+page.svelte
+++ src/routes/drafts/[slug]/+page.svelte
@@ -9,13 +9,26 @@
 
 	export let data
 
-	const { content, frontmatter } = data.post
-
-	let editUrl = `${config.fileUrl}/${frontmatter.slug}/${frontmatter.slug}.md`
+	let editUrl = `${config.fileUrl}/${data.frontmatter.slug}/${data.frontmatter.slug}.md`
+	let image = `${config.postImage}${encodeURIComponent(data.frontmatter.title)}.png`
 </script>
 
 <svelte:head>
-	<title>{frontmatter.title}</title>
+	<title>{data.frontmatter.title}</title>
+
+	<meta content={data.frontmatter.description} name="description" />
+
+	<meta content={data.frontmatter.title} property="og:title" />
+	<meta content={image} property="og:image" />
+	<meta content={config.siteUrl} property="og:url" />
+	<meta content={data.frontmatter.description} property="og:description" />
+	<meta content={config.siteName} property="og:site_name" />
+
+	<meta content={config.twitterHandle} name="twitter:creator" />
+	<meta content="summary_large_image" name="twitter:card" />
+	<meta content={data.frontmatter.title} name="twitter:title" />
+	<meta content={data.frontmatter.description} name="twitter:description" />
+	<meta content={image} name="twitter:image" />
 </svelte:head>
 
 <Clipboard />
@@ -26,11 +39,13 @@
 
 	<article class="prose">
 		<header>
-			<h1 class="title">{frontmatter.title}</h1>
-			<p class="published">Published {formatDate(frontmatter.published)}</p>
+			<h1 class="title">{data.frontmatter.title}</h1>
+			<p class="published">
+				Published {formatDate(data.frontmatter.published)}
+			</p>
 		</header>
 
-		{@html content}
+		<svelte:component this={data.component} />
 	</article>
 
 	<div class="cards">
