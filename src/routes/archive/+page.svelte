cef0480c934d8dcf7d004ec5d4c608a1b6374142
mattcroat <mattcroat.dev@gmail.com>
2024-06-23 19:47:20 +0200

    refactor: archive and drafts

--- src/routes/archive/+page.svelte
+++ src/routes/archive/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Heading from '$lib/ui/heading.svelte'
-	import Posts from '$lib/ui/posts.svelte'
+	import { fade } from 'svelte/transition'
 
 	export let data
 </script>
@@ -12,22 +12,75 @@
 
 <Heading>Archive</Heading>
 
-<Posts posts={data.posts}>
-	<div class="container" slot="title">
+<section>
+	<div class="container">
 		<h3>Posts</h3>
 		<div>
 			<span class="results">{data.posts.length}</span> results
 		</div>
 	</div>
-</Posts>
+
+	<div class="posts">
+		{#each data.posts as post, i}
+			<div
+				in:fade={{
+					duration: 300,
+					delay: i < 10 ? 100 * i : 100 * 4,
+				}}
+			>
+				<a href="/{post.slug}">
+					<article class="post">
+						<div class="details">
+							<span class="title">{post.title}</span>
+						</div>
+					</article>
+				</a>
+			</div>
+		{/each}
+	</div>
+</section>
 
 <style>
-	.container {
-		display: flex;
-		justify-content: space-between;
+	section {
+		max-width: 740px;
+		margin-inline: auto;
+		margin-block-start: var(--spacing-64);
+
+		& .container {
+			display: flex;
+			justify-content: space-between;
+
+			& .results {
+				font-weight: 700;
+			}
+		}
 	}
 
-	.results {
-		font-weight: 700;
+	.posts {
+		margin-top: var(--spacing-64);
+
+		& a::before {
+			content: none;
+		}
+
+		& .post {
+			margin-block-start: var(--spacing-32);
+			padding-block-end: var(--spacing-32);
+			border-bottom: 1px solid var(--clr-menu-border);
+
+			& .title {
+				font-family: var(--font-sans);
+				font-size: clamp(var(--font-24), 4vw, var(--font-32));
+				font-weight: 500;
+				line-height: 40px;
+				text-transform: capitalize;
+			}
+		}
+	}
+
+	html[data-font='dyslexic'] .post .title {
+		font-family: var(--font-dyslexic);
+		font-size: var(--font-24);
+		line-height: 32px;
 	}
 </style>
--- src/routes/drafts/+page.svelte
+++ src/routes/drafts/+page.svelte
@@ -14,25 +14,24 @@
 
 <section>
 	<div class="container">
-		<h3>Posts</h3>
+		<h3>Drafts</h3>
 		<div>
 			<span class="results">{data.posts.length}</span> results
 		</div>
 	</div>
 
-	<div class="cards">
+	<div class="posts">
 		{#each data.posts as post, i}
 			<div
 				in:fade={{
 					duration: 300,
-					delay: i < 4 ? 100 * i : 100 * 4,
+					delay: i < 10 ? 100 * i : 100 * 4,
 				}}
 			>
 				<a href="/drafts/{post.slug}">
-					<article class="card">
+					<article class="post">
 						<div class="details">
 							<span class="title">{post.title}</span>
-							<p class="description">{post.description}</p>
 						</div>
 					</article>
 				</a>
@@ -42,79 +41,44 @@
 </section>
 
 <style>
-	.container {
-		display: flex;
-		justify-content: space-between;
-
-		& .results {
-			font-weight: 700;
-		}
-	}
 	section {
-		margin-top: var(--spacing-64);
-	}
+		max-width: 740px;
+		margin-inline: auto;
+		margin-block-start: var(--spacing-64);
 
-	.cards {
-		display: grid;
-		gap: var(--spacing-32);
-		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-		margin-top: var(--spacing-32);
+		& .container {
+			display: flex;
+			justify-content: space-between;
 
-		& a {
-			display: block;
-			color: inherit;
-			font-weight: inherit;
-			text-decoration: none;
-			transition: color 0.3s ease;
-
-			&::before {
-				content: none;
+			& .results {
+				font-weight: 700;
 			}
 		}
 	}
 
-	.card {
-		height: 420px;
-		padding: var(--spacing-32);
-		background-image: var(--clr-card-bg);
-		border-top: 1px solid var(--clr-menu-border);
-		border-left: 1px solid var(--clr-menu-border);
-		border-radius: var(--rounded-20);
-		box-shadow: var(--shadow-sm);
-		transition:
-			transform 0.2s ease-in-out,
-			box-shadow 0.3s ease,
-			outline 0.1s ease;
+	.posts {
+		margin-top: var(--spacing-64);
 
-		&:hover {
-			transform: scale(1.02);
-			box-shadow:
-				var(--shadow-md),
-				0 0 0 4px var(--clr-primary);
+		& a::before {
+			content: none;
 		}
 
-		& .details {
-			height: 100%;
-			display: grid;
-		}
+		& .post {
+			margin-block-start: var(--spacing-32);
+			padding-block-end: var(--spacing-32);
+			border-bottom: 1px solid var(--clr-menu-border);
 
-		& .title {
-			font-family: var(--font-sans);
-			font-size: clamp(var(--font-24), 4vw, var(--font-32));
-			font-weight: 700;
-			line-height: 40px;
-			text-transform: capitalize;
-			text-wrap: balance;
-		}
-
-		& .description {
-			margin-top: var(--spacing-8);
-			color: var(--clr-card-txt);
-			align-self: end;
+			& .title {
+				font-family: var(--font-sans);
+				font-size: clamp(var(--font-24), 4vw, var(--font-32));
+				font-weight: 500;
+				line-height: 40px;
+				text-transform: capitalize;
+			}
 		}
 	}
 
-	html[data-font='dyslexic'] .card .title {
+	html[data-font='dyslexic'] .post .title {
 		font-family: var(--font-dyslexic);
 		font-size: var(--font-24);
 		line-height: 32px;
